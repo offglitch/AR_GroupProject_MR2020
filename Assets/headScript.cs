a042acb64c2ba31f75884130e05666d89e95d531
Kevin Agnes <kevinagnes@gmail.com>
2020-11-28 18:05:22 +0000

    updating

---
+++ Assets/headScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headScript : MonoBehaviour
+{
+    public GameObject ARcamera; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        ARcamera = GameObject.Find("AR Camera");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(ARcamera.transform);
+    }
+}
